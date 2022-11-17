f09f822c1b0f806e6a3590a922bd0ef6551493a8
Jenn_07 <cuevasjennifer07@gmail.com>
2022-11-17 17:08:15 -0500

    Crud eliminar

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-agregar',
@@ -38,7 +39,8 @@ export class AgregarComponent implements OnInit {
 
   constructor( private heroesService: HeroesService,
                 private activatedRoute: ActivatedRoute,
-                private router: Router) { }
+                private router: Router,
+                private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
 
@@ -64,15 +66,34 @@ export class AgregarComponent implements OnInit {
     if ( this.heroe.id ) {
       //Actualizar
       this.heroesService.actualizarHeroe( this.heroe )
-        .subscribe( heroe => console.log( 'Actualizando', heroe ) )
+        .subscribe( heroe => this.mostrarSnakbar('Registro actualizado'));
     } else {
       //Crear
       this.heroesService.agregarHeroe( this.heroe )
       .subscribe( heroe => {
         this.router.navigate(['/heroes/editar', heroe.id ]);
+        this.mostrarSnakbar('Registro creado');
       })
-    }
-
-    
+    }    
   }
+
+  borrarHeroe() {
+
+    this.heroesService.borrarHeroe( this.heroe.id! )
+      .subscribe( resp => {
+
+        this.router.navigate(['/heroes'])
+      });
+
+  }
+
+  mostrarSnakbar( mensaje: string ) {
+
+    this.snackBar.open( mensaje, 'ok!', { 
+      duration: 2500
+    });
+  }
+
+
+  
 }
